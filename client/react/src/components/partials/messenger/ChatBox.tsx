6e88f89676888516c87fe47191cfb3d04cea323a
Petya29 <petya29avramenko@gmail.com>
2023-06-18 16:32:51 +0200

    Added ChatBox structure and init styling

--- client/react/src/components/partials/messenger/ChatBox.tsx
+++ client/react/src/components/partials/messenger/ChatBox.tsx
@@ -2,13 +2,32 @@ import { Message } from "./Message";
 
 export const ChatBox = () => {
 	return (
-		<div className="basis-1/2">
-			<div>
+		<div className="basis-1/2 mx-5">
+			<div className="h-[88%] overflow-y-scroll pr-2">
+				<Message />
+				<Message isOwnMessage />
 				<Message />
 				<Message />
 				<Message />
+				<Message />
+				<Message />
+				<Message />
+				<Message />
+				<Message />
+				<Message />
+				<Message />
+				<Message isOwnMessage />
 			</div>
-			<div></div>
+			<div className="flex mt-5 items-center justify-between">
+				<textarea
+					className="w-[80%] border p-1 outline-none"
+					rows={3}
+					placeholder="Write a message ..."
+				></textarea>
+				<button className="w-16 h-9 rounded cursor-pointer text-white bg-teal-400">
+					Send
+				</button>
+			</div>
 		</div>
 	);
 };
--- client/react/src/components/partials/messenger/Message.tsx
+++ client/react/src/components/partials/messenger/Message.tsx
@@ -1,15 +1,32 @@
-export const Message = () => {
+type MessageProps = {
+	isOwnMessage?: boolean;
+};
+
+export const Message = ({ isOwnMessage = false }: MessageProps) => {
 	return (
-		<div className="flex flex-col">
-			<div>
+		<div
+			className={["flex flex-col mt-5", isOwnMessage ? "items-end" : ""]
+				.join(" ")
+				.trim()}
+		>
+			<div className="flex">
 				<img
-					className="w-8 h-8 rounded-full object-cover"
-					src="https://static.vecteezy.com/system/resources/previews/008/442/086/original/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg"
+					className="w-8 h-8 mr-2 rounded-full object-cover"
+					src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
 					alt="user-avatar"
 				/>
-				<p>Hello, my dear friend!</p>
+				<span
+					className={[
+						"max-w-xs p-3 rounded-md text-white",
+						isOwnMessage ? "bg-slate-500" : "bg-slate-700",
+					]
+						.join(" ")
+						.trim()}
+				>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+					<div className="text-right text-xs">3:53 PM</div>
+				</span>
 			</div>
-			<div>1 hour ago</div>
 		</div>
 	);
 };
